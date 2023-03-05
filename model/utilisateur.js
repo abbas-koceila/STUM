13cd8d1ee68576c0a5b12c309c403038a3268c79
Koceila Abbas <2702934@collegelacite.ca>
2023-03-05 18:25:36 -0500

    ADD YOUNISSS STAFF ADMIN

--- model/utilisateur.js
+++ model/utilisateur.js
@@ -9,6 +9,7 @@ export const getUserId = async (courriel) => {
     return newId.lastID;
 }
 
+
 export const getUtilisateurById = async (id) => {
     let connexion = await promesseConnexion;
 
@@ -16,6 +17,7 @@ export const getUtilisateurById = async (id) => {
 
     return utilisateur;
 }
+
 
 
 
@@ -65,3 +67,49 @@ export const getTypeAcces=async (nomUtilisateur)=>{
     );
     return Typeutilisateur;
 }
+export const getPatient=async ()=>{
+    let connexion = await promesseConnexion;
+
+   let utilisateur= await  connexion.all(
+        `SELECT ur.id_utilisateur, ur.date_urgence, ur.niveau_urgence, ut.nom , ut.prenom, rdv.date_rendez_vous FROM urgence ur
+        INNER JOIN utilisateur ut 
+        ON ur.id_utilisateur = ut.id_utilisateur
+        INNER JOIN rendez_vous rdv  
+        ON ur.id_utilisateur = rdv.id_utilisateur
+        WHERE ur.etat_urgence=1
+        ORDER BY rdv.date_rendez_vous DESC`,
+    );
+    for (let i = 0; i < utilisateur.length; i++) {
+        let dateur=new Date(utilisateur[i].date_urgence);
+        utilisateur[i].date_urgence =  dateur.toDateString()+", "+dateur.getHours() + ":" + dateur.getMinutes();
+        let daterdv=new Date(utilisateur[i].date_rendez_vous);
+        utilisateur[i].date_rendez_vous =  daterdv.toDateString()+", "+daterdv.getHours() + ":" + daterdv.getMinutes();
+       
+    }
+    
+    return utilisateur;
+}
+export const getFormulaire=async (id_user)=>{
+    let connexion = await promesseConnexion;
+
+   let formulaire= await  connexion.all(
+        `SELECT ut.nom, ut.prenom , ut.courriel, p.numero_carte_sante,p.numero_tel,
+        f.date_debut_symptomes, f.description, f.symptomes,
+        f.medical_condition, f.hospital_history, f.medication_history, f.last_meal,
+        f.tete_gauche,f.tete_droite,f.cou_gauche, f.cou_droite, f.epaule_gauche,
+        f.epaule_droite, f.poitrine_gauche, f.poitrine_droite, f.coude_gauche,
+        f.coude_droite, f.main_et_poignet_gauche, f.main_et_poignet_droit, f.hanche_gauche,
+        f.hanche_droite, f.cuisse_gauche, f.cuisse_droite, f.genou_gauche, f.genou_droit,
+        f.jambe_gauche, f.jambe_droite, f.pied_gauche, f.pied_droite,
+        f.douleur_present, f.douleur8jours, f.douleur_intense
+        FROM utilisateur ut
+        INNER JOIN patient p 
+        ON p.id_utilisateur=ut.id_utilisateur
+        INNER JOIN formulaire f
+        On ut.id_utilisateur = f.id_utilisateur 
+        WHERE ut.id_utilisateur = ?`,
+        [id_user]
+    );
+    return formulaire;
+}
+
