8f37eda2860fa08e6d76e7747a64381ec2f14ec2
yor-guen1 <2703066@collegelacite.ca>
2023-03-12 22:41:10 -0400

    - modifier formulaire ADMIN

--- public/js/Admin.js
+++ public/js/Admin.js
@@ -1,9 +1,36 @@
+
 //il faut faire un boucle pour le tableaux des button retournees 
 let edits = document.querySelectorAll('.edit');
 for (const edit of edits) {
-    edit.addEventListener('click',(event)=>{
-        console.log(event.currentTarget);       
-            window.location.replace(`/modification/${event.currentTarget.dataset.id}`)
-    })
+    edit.addEventListener('click', (event) => {
+        console.log(event.currentTarget);
+        window.location.replace(`/modification/${event.currentTarget.dataset.id}`)
+    });
+}
+
+let deleteBtns = document.querySelectorAll('.delete')
+for (const deleteBtn of deleteBtns) {
+    deleteBtn.addEventListener('click', async (event) => {
+        event.preventDefault();
+        console.log(event.currentTarget.dataset.id)
+        let data = {
+            id: event.currentTarget.dataset.id
+        }
+        try {
+            let response = await fetch('/deleterdv', {
+                method: 'DELETE',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(data)
+            });
+            if (response.ok) {
+                console.log('Rendez-vous deleted successfully');
+                location.reload();
+            } else {
+                console.error('Failed to delete Rendez-vous');
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    });
 }
 
---
+++ public/js/changeInfo.js
@@ -0,0 +1,26 @@
+import formToObject from './formToObject.js';
+let formInfo = document.getElementById('changeInfo_form');
+
+
+
+
+// submit form 
+
+formInfo.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    let data = formToObject(formInfo);
+    let res = await fetch('/changeInfo', {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    });
+    if (res.ok) {
+        console.log(' vos informations sont changés avec succes');
+    }
+    else if (res.status === 409) {
+        console.log('erreur de conflict ');
+        document.getElementById("email").style.borderColor = "red";
+        document.getElementById("error-email").innerHTML = "Cette adresse e-mail existe déjà dans notre base de données.";
+    }
+});
+
