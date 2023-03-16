dd30b452d629264c743b749e986c9b24c215f1e7
Koceila Abbas <2702934@collegelacite.ca>
2023-03-16 16:40:23 -0400

    confirm card

--- public/js/urgence.js
+++ public/js/urgence.js
@@ -1,32 +1,46 @@
 const annulerRdvButtons = document.querySelectorAll('.rdvfutur');
 
+//confirm button script 
+const confirmModal = document.querySelector('.confirm-modal');
+const confirmButton = document.querySelector('#confirm-button');
+const cancelButton = document.querySelector('#cancel-button');
+
 annulerRdvButtons.forEach(button => {
-  button.addEventListener('click', async () => {
+  button.addEventListener('click', () => {
+  
+    confirmModal.style.display = 'flex';
+    confirmButton.dataset.id = button.dataset.id;
 
-    console.log("akhirrclick annuler");
-    try {
-      const confirmed = confirm("Êtes-vous sûr de vouloir d'annuler ce rendez-vous ?");
-      if (!confirmed) {
-        return; // Sortir de la fonction si l'utilisateur a annulé
-      }
-      const response = await fetch('/deleterdv', {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          id: button.dataset.id
-        })
-      });
-
-      if (response.ok) {
-        console.log('Rendez-vous deleted successfully');
-        location.reload(); 
-      } else {
-        console.error('Failed to delete Rendez-vous');
-      }
-    } catch (error) {
-      console.error(error);
-    }
+    console.log('waggiii id urgence confirm button',  confirmButton.dataset.id);
   });
+});
+
+
+confirmButton.addEventListener('click', async () => {
+  console.log("akhirrclick annuler");
+  try {
+    const response = await fetch('/deleterdv', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        id: confirmButton.dataset.id
+      })
+    });
+
+    if (response.ok) {
+      console.log('Rendez-vous deleted successfully');
+      location.reload(); 
+    } else {
+      console.error('Failed to delete Rendez-vous');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+  confirmModal.style.display = 'none';
+});
+
+cancelButton.addEventListener('click', () => {
+  confirmModal.style.display = 'none';
 });
