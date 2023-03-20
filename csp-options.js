37af3fb2f81d102dee6c8c776d0b1a0101a7e359
koceila abbas <73756505+abbas-koceila@users.noreply.github.com>
2023-03-20 18:48:38 -0400

    Update csp-options.js

--- csp-options.js
+++ csp-options.js
@@ -4,8 +4,8 @@ if(process.env.NODE_ENV === 'development'){
     options = {
         contentSecurityPolicy: {
             directives: { 
-                defaultSrc: ["'self'"],
-                baseUri: ["'self'"],
+               defaultSrc: ["'self'","https://www.donneesquebec.ca/recherche/api/3/action/datastore_search_sql?sql=SELECT%20*%20from%20%22b256f87f-40ec-4c79-bdba-a23e9c50e741%22%20WHERE%20_id%20%3D73"],
+            
                 blockAllMixedContent: [],
                 fontSrc: ["'self'", "https:", "data:"],
                 frameAncestors: ["'self'"],
@@ -21,3 +21,4 @@ if(process.env.NODE_ENV === 'development'){
 }
 
 export default options;
+
